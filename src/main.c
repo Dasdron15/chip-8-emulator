21a5807124f14d40f06657dc7c499bbe8b151526
Dasdron15 <ogurechik760@gmail.com>
2025-03-31 00:52:22 +0200

    Added window

---
+++ src/main.c
@@ -0,0 +1,53 @@
+#include <SDL_events.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+
+#ifdef _WIN32
+#include <SDL/SDL.>
+#else
+#include <SDL2/SDL.h>
+#endif
+
+#define WIDTH 800
+#define HEIGHT 600
+#define DELAY 3000
+
+SDL_Window *window;
+
+void cleanupSDL();
+
+int main(int argc, char* argv[]) {
+    SDL_Window *window = NULL;
+    SDL_Event event;
+    bool quit = false;
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        fprintf(stderr, "Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    window = SDL_CreateWindow("Chip-8", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE);
+
+    if (window == NULL) {
+        fprintf(stderr, "Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    while (!quit) {
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+    }
+
+    cleanupSDL();
+
+    return 0;
+}
+
+void cleanupSDL() {
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
